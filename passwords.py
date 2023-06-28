4fb02cb757aac6af20cc2f1b2a88cd9b106e2ae6
Jonah Wang <imargo4507@outlook.com>
2023-06-28 19:09:13 -0400

    implemented the password system and ui for the password system

--- passwords.py
+++ passwords.py
@@ -1,4 +1,4 @@
-
+from flask import session
 passwords = {
     "jonah": "4507",
     "chaz": None,
@@ -10,3 +10,9 @@ passwords = {
 def get_password(student_name):
     password = passwords[student_name]
     return password
+
+def is_unlocked(student_name):
+    if session.get(f"{student_name}_password_entered") is True:
+        return True
+    else:
+        return False
